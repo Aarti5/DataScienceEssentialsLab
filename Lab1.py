76af864444fdd0cee02ee99181e8126619e16d2f
Aarti5 <jaiswal.aarti5@gmail.com>
2015-09-09 15:35:47 -0400

    Python program code for printing a line
    The code prints  "hello world"

---
+++ Lab1.py
@@ -0,0 +1,7 @@
+print"Hello World!"
+exit();
+"""print is used as a function in python 3 version but in python 2 version it is used as a statement and directly prints whatever given
+python 3 version
+print("Hello World!")
+exit();
+"""
